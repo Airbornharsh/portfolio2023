0f4270c3a461fe968537b01545ae655b4f3e48ae
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-10-31 22:14:28 +0530

    Added Close Button Feature

--- src/components/terminal/Body.tsx
+++ src/components/terminal/Body.tsx
@@ -1,18 +1,14 @@
-import { useRef } from "react";
 import CmdInput from "../body/CmdInput";
 import CmdOutput from "../body/CmdOutput";
 import CmdSuggestion from "../body/CmdSuggestion";
 
-const Body = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
+interface BodyProps {
+  inputRef: React.RefObject<HTMLInputElement>;
+}
 
+const Body = ({ inputRef }: BodyProps) => {
   return (
-    <div
-      className="w-full h-full px-4 text-white overflow-x-clip overflow-y-auto scroll-bar-hide"
-      onClick={() => {
-        inputRef.current?.focus();
-      }}
-    >
+    <div className="w-full h-full px-4 text-white overflow-x-clip overflow-y-auto scroll-bar-hide">
       <p className="font-medium text-sm">Type "help" for help</p>
       <ul className="flex flex-col items-start">
         <CmdOutput />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,53 @@
-import { useContext } from "react";
-import Body from "../components/terminal/Body";
+import { useContext, useRef } from "react";
 import Head from "../components/terminal/Head";
+import Body from "../components/terminal/Body";
 import Context from "../Context/Context";
 
 const Home = () => {
   const Ctx = useContext(Context);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <div className="w-screen h-screen bg-color1 flex justify-center items-center overflow-hidden">
-      {Ctx.closed || (
-        <div
-          className="w-[96vw]  bg-color1 h-[96vh] rounded-2xl shadow-sm shadow-color3 terminal-Container border-[0.2rem] border-color3 overflow-hidden relative"
-          style={{
-            maxWidth: Ctx.maximized ? "100vw" : "60rem",
-            maxHeight: Ctx.maximized ? "100%" : "40rem",
-            transition: "all 0.2s ease-in-out",
-          }}
-        >
-          <Head />
-          <Body />
-        </div>
-      )}
+    <div className="w-screen h-screen bg-color1 overflow-hidden">
+      <div
+        className="bg-color1  rounded-2xl shadow-sm shadow-color3 terminal-Container border-[0.2rem] border-color3 overflow-hidden fixed left-[50%]"
+        style={{
+          maxWidth: Ctx.maximized ? "100%" : "60rem",
+          maxHeight: Ctx.maximized ? "100%" : "40rem",
+          transition: "all 0.2s ease-in-out",
+          width: !Ctx.closed ? "96vw" : "3rem",
+          height: !Ctx.closed ? "96vh" : "3rem",
+          bottom: Ctx.closed ? "1rem" : "unset",
+          top: Ctx.closed ? "unset" : "50%",
+          transform: Ctx.closed ? "unset" : "translate(-50%,-50%)",
+          zIndex: Ctx.closed ? 100 : 0,
+        }}
+      >
+        {Ctx.closed ? (
+          <div
+            className="w-full h-full flex justify-center items-center"
+            onClick={() => {
+              Ctx.handleClose(false);
+            }}
+          >
+            <img
+              src="/jpeg/airbornharsh.jpeg"
+              alt="photo"
+              className="h-8 w-8"
+            />
+          </div>
+        ) : (
+          <div
+          className="w-full h-full flex flex-col"
+            onClick={() => {
+              inputRef.current?.focus();
+            }}
+          >
+            <Head />
+            <Body inputRef={inputRef} />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
