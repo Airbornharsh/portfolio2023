00f103cbb72945b6d7437d8e7c3ff70c5e5680e7
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-11-02 06:22:11 +0530

    Added Title

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import Helmet from "react-helmet";
 import App from "./App.tsx";
 import "./index.css";
 import { ContextProvider } from "./Context/ContextProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Helmet>
+      <title>Harsh Keshri</title>
+    </Helmet>
     <ContextProvider>
       <BrowserRouter>
         <App />
