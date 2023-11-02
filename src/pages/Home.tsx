b3a6256c40b94da4af1d322f9089807fc1937123
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-11-02 07:03:50 +0530

    Made Draggble

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import Head from "../components/terminal/Head";
 import Body from "../components/terminal/Body";
 import Context from "../Context/Context";
@@ -6,11 +6,34 @@ import Context from "../Context/Context";
 const Home = () => {
   const Ctx = useContext(Context);
   const inputRef = useRef<HTMLInputElement>(null);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isDragging, setIsDragging] = useState(false);
+  const [dragStart, setDragStart] = useState({ x: 0, y: 0 });
+
+  const handleMouseDown = (e: { clientX: number; clientY: number }) => {
+    setIsDragging(true);
+    setDragStart({
+      x: e.clientX - position.x,
+      y: e.clientY - position.y,
+    });
+  };
+
+  const handleMouseMove = (e: { clientX: number; clientY: number }) => {
+    if (isDragging) {
+      const newX = e.clientX - dragStart.x;
+      const newY = e.clientY - dragStart.y;
+      setPosition({ x: newX, y: newY });
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
 
   return (
     <div className="w-screen h-screen bg-color1 overflow-hidden">
       <div
-        className="bg-color1  rounded-2xl shadow-sm shadow-color3 terminal-Container border-[0.2rem] border-color3 overflow-hidden fixed left-[50%]"
+        className="bg-color1  rounded-2xl shadow-sm shadow-color3 terminal-Container border-[0.2rem] border-color3 overflow-hidden fixed "
         style={{
           maxWidth: Ctx.maximized ? "100%" : "60rem",
           maxHeight: Ctx.maximized ? "100%" : "40rem",
@@ -18,10 +41,16 @@ const Home = () => {
           width: !Ctx.closed ? "96vw" : "3rem",
           height: !Ctx.closed ? "96vh" : "3rem",
           bottom: Ctx.closed ? "1rem" : "unset",
-          top: Ctx.closed ? "unset" : "50%",
+          top: Ctx.closed ? "unset" : `calc(${position.y}px + 50%)`,
           transform: Ctx.closed ? "translateX(-50%)" : "translate(-50%,-50%)",
           zIndex: Ctx.closed ? 100 : 0,
+          left: Ctx.closed ? "50%" : `calc(${position.x}px + 50%)`,
+          // top: position.y,
+          cursor: isDragging ? "grabbing" : "grab",
         }}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
       >
         {Ctx.closed ? (
           <div
