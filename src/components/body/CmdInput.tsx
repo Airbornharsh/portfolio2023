e8f5aa49c31edd3fa9b64822f008faf026342570
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-11-02 06:32:39 +0530

    Opacity 0 for the input

--- src/components/body/CmdInput.tsx
+++ src/components/body/CmdInput.tsx
@@ -100,7 +100,7 @@ const CmdInput = ({ inputRef }: CmdInputProps) => {
         }}
       >
         <input
-          className="text-black w-1"
+          className="text-black w-1 opacity-0"
           autoFocus={true}
           ref={inputRef}
           type="text"
